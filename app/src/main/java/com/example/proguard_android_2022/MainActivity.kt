a5643103e72c45e3016b914910d62862a712ac2f
BrandonQ <brandonquintanillaq@gmail.com>
2022-11-18 11:12:49 -0500

    Project creation

---
+++ app/src/main/java/com/example/proguard_android_2022/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.proguard_android_2022
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.proguard_android_2022.user.UserPrueba
+
+class MainActivity : AppCompatActivity() {
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
