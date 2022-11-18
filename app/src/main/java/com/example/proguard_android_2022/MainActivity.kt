ef623402c5e1e7872c4803bada6f059fcad7988e
BrandonQ <brandonquintanillaq@gmail.com>
2022-11-18 11:13:41 -0500

    ADD -> experimentation code

--- app/src/main/java/com/example/proguard_android_2022/MainActivity.kt
+++ app/src/main/java/com/example/proguard_android_2022/MainActivity.kt
@@ -6,13 +6,14 @@ import com.example.proguard_android_2022.user.UserPrueba
 
 class MainActivity : AppCompatActivity() {
 
-
-
+    var user: UserPrueba? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        user = UserPrueba()
+        user?.getEmailProfessor()
+        user?.setEmailProfessor("alberto")
     }
 }
